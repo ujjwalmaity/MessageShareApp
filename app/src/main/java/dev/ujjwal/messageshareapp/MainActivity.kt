268e670fe48425bad9ec8050bc575a14ee65b541
ujjwal <ujjwalmaity98@gmail.com>
2019-11-06 11:34:27 +0530

    Show toast & log on button clicked

--- app/src/main/java/dev/ujjwal/messageshareapp/MainActivity.kt
+++ app/src/main/java/dev/ujjwal/messageshareapp/MainActivity.kt
@@ -2,11 +2,19 @@ package dev.ujjwal.messageshareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_showToast.setOnClickListener {
+            Log.i("MainActivity", "Button clicked !")
+            Toast.makeText(applicationContext, "Button clicked !", Toast.LENGTH_SHORT).show()
+        }
     }
 }
